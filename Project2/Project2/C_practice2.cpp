e77fa2e441b5a144a5c0cab95d7edfecff73fd60
glzuk <dwc20030810@gmail.com>
2026-09-22 19:29:04 +0900

    practice2

---
+++ Project2/Project2/C_practice2.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    char name[] = "김세훈";
+    int id = 2026440019;
+    double grade = 3.33;
+
+    printf("서울시립대학교\n");
+    printf("전자전기컴퓨터공학부\n");
+    printf("이름 : %s\n", name);
+    printf("학번 : %d\n", id);
+    printf("학점 : %.2f", grade);
+
+    return 0;
+}
